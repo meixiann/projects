bbe48c9ce60fdab5c12a3170e8e4e96b8769ba0f
root <root@localhost.localdomain>
2020-11-23 03:29:37 -0500

    updated files

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var dbconn = mysql.createConnection({
-    host     : 'localhost',
+    host     : '192.168.80.176',
     user     : 'root',
     password : 'toor',
     database : 'mydb'
