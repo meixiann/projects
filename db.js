444f805fcf4a58751eca857ecc167a485abeb50f
root <root@localhost.localdomain>
2021-01-25 03:54:38 -0500

    test

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var dbconn = mysql.createConnection({
-    host     : 'local-host',
+    host     : '192.168.80.134',
     user     : 'root',
     password : 'toor',
     database : 'mydb'
